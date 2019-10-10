4b76fcaad932a24a1a77428ba4c62ccebe685343
plhnk <paul.hanaoka@gmail.com>
2019-10-10 11:20:10 -0700

    update icons in CMS

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,68 +1,68 @@
 import S from '@sanity/desk-tool/structure-builder'
-import { MdBusiness, MdSettings } from 'react-icons/md'
-import { FaFile } from 'react-icons/fa'
+import { MdWeb, MdSettings } from "react-icons/md";
+import { FaRebel } from 'react-icons/fa'
 
 const hiddenTypes = ['category', 'myInfo', 'page', 'person', 'project', 'siteSettings']
 
 export default () =>
   S.list()
-    .title('Content')
+    .title("Content")
     .items([
       S.listItem()
-        .title('Site Settings')
+        .title("Site Settings")
         .child(
           S.editor()
-            .id('siteSettings')
-            .schemaType('siteSettings')
-            .documentId('siteSettings')
+            .id("siteSettings")
+            .schemaType("siteSettings")
+            .documentId("siteSettings")
         )
         .icon(MdSettings),
       S.listItem()
-        .title('My Info')
+        .title("My Info")
         .child(
           S.editor()
-            .id('myInfo')
-            .schemaType('myInfo')
-            .documentId('myInfo')
+            .id("myInfo")
+            .schemaType("myInfo")
+            .documentId("myInfo")
         )
-        .icon(MdBusiness),
+        .icon(FaRebel),
       S.listItem()
-        .title('Projects')
-        .schemaType('project')
-        .child(S.documentTypeList('project')),
+        .title("Projects")
+        .schemaType("project")
+        .child(S.documentTypeList("project")),
       S.listItem()
-        .title('Pages')
+        .title("Pages")
         .child(
           S.list()
-            .title('Pages')
+            .title("Pages")
             .items([
               S.listItem()
-                .title('About')
+                .title("About")
                 .child(
                   S.editor()
-                    .id('aboutPage')
-                    .schemaType('page')
-                    .documentId('about')
+                    .id("aboutPage")
+                    .schemaType("page")
+                    .documentId("about")
                 )
-                .icon(FaFile),
+                .icon(MdWeb),
               S.listItem()
-                .title('Contact')
+                .title("Contact")
                 .child(
                   S.editor()
-                    .id('contactPage')
-                    .schemaType('page')
-                    .documentId('contact')
+                    .id("contactPage")
+                    .schemaType("page")
+                    .documentId("contact")
                 )
-                .icon(FaFile)
+                .icon(MdWeb)
             ])
         ),
       S.listItem()
-        .title('People')
-        .schemaType('person')
-        .child(S.documentTypeList('person').title('People')),
+        .title("People")
+        .schemaType("person")
+        .child(S.documentTypeList("person").title("People")),
       S.listItem()
-        .title('Categories')
-        .schemaType('category')
-        .child(S.documentTypeList('category').title('Categories')),
+        .title("Categories")
+        .schemaType("category")
+        .child(S.documentTypeList("category").title("Categories")),
       ...S.documentTypeListItems().filter(listItem => !hiddenTypes.includes(listItem.getId()))
-    ])
+    ]);
